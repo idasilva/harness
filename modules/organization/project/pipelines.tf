a94fb107a66be7d8acdd752e00b75b1f47538534
isc <isaias.carvalho986@gmail.com>
2025-11-28 11:46:06 -0300

    add defauls pipelines

---
+++ modules/organization/project/pipelines.tf
@@ -0,0 +1,12 @@
+resource "harness_platform_pipeline" "hello_world" {
+  identifier = "hello_world"
+  org_id     = var.organization_id
+  project_id = var.project
+  name       = "Hello World Pipeline"
+  
+  yaml = templatefile("${path.module}/templates/hello-world-pipeline.yaml", {
+    PROJECT_ID_PLACEHOLDER = var.project
+    ORG_ID_PLACEHOLDER     = var.organization_id
+  })
+
+}
