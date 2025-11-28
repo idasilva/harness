318348b2b75491f0171ef8a7d42ef4d8bb41b3d6
isc <isaias.carvalho986@gmail.com>
2025-11-28 11:53:37 -0300

    change pipelines names

--- modules/organization/project/pipelines.tf
+++ modules/organization/project/pipelines.tf
@@ -2,7 +2,7 @@ resource "harness_platform_pipeline" "hello_world" {
   identifier = "hello_world"
   org_id     = var.organization_id
   project_id = var.project
-  name       = "Hello World Pipeline"
+  name       = "${var.project}-pipeline"
   
   yaml = templatefile("${path.module}/templates/hello-world-pipeline.yaml", {
     PROJECT_ID_PLACEHOLDER = var.project
