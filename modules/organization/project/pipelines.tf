a90ad7a9e0f273ca7b873b8019ac64bb437c84ea
isc <isaias.carvalho986@gmail.com>
2025-11-28 16:23:39 -0300

    add default pipelines

--- modules/organization/project/pipelines.tf
+++ modules/organization/project/pipelines.tf
@@ -2,11 +2,27 @@ resource "harness_platform_pipeline" "hello_world" {
   identifier = "hello_world"
   org_id     = var.organization_id
   project_id = var.project
-  name       = "${var.project}-pipeline"
+  name       = "Hello World Pipeline"
   
   yaml = templatefile("${path.module}/templates/hello-world-pipeline.yaml", {
-    PROJECT_ID_PLACEHOLDER = var.project
-    ORG_ID_PLACEHOLDER     = var.organization_id
+    PROJECT_ID_PLACEHOLDER = var.project,
+    ORG_ID_PLACEHOLDER     = var.organization_id,
+    PIPELINE_NAME          = "Hello World Pipeline",
+    PIPELINE_IDENTIFIER    = "hello_world"
   })
+}
 
+
+resource "harness_platform_pipeline" "ci_pipeline" {
+  identifier = "ci_build_push"
+  org_id     = var.organization_id
+  project_id = var.project
+  name       = "CI Pipeline - Build and Push"
+  
+  yaml = templatefile("${path.module}/templates/ci-pipeline.yaml", {
+    PROJECT_ID_PLACEHOLDER = var.project,
+    ORG_ID_PLACEHOLDER     = var.organization_id,
+    PIPELINE_NAME          = "CI Pipeline - Build and Push"
+    PIPELINE_IDENTIFIER    = "ci_build_push"
+  })
 }
