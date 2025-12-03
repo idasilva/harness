6e433bbd7e255964cce9202caa10381520b3ec25
isc <isaias.carvalho986@gmail.com>
2025-12-03 11:33:16 -0300

    remove unused pipe

--- modules/organization/project/pipelines.tf
+++ modules/organization/project/pipelines.tf
@@ -1,19 +1,3 @@
-resource "harness_platform_pipeline" "hello_world" {
-  identifier = "hello_world"
-  org_id     = var.organization_id
-  project_id = var.project
-  name       = "Hello World Pipeline"
-  
-  yaml = templatefile("${path.module}/templates/hello-world-pipeline.yaml", {
-    PROJECT_ID_PLACEHOLDER = var.project,
-    ORG_ID_PLACEHOLDER     = var.organization_id,
-    PIPELINE_NAME          = "Hello World Pipeline",
-    PIPELINE_IDENTIFIER    = "hello_world"
-  })
-    depends_on = [ harness_platform_project.ck_project ]
-}
-
-
 resource "harness_platform_pipeline" "ci_pipeline" {
   identifier = "ci_build_push"
   org_id     = var.organization_id
