4244cce3236677692c5725f042f7d81ee095c97a
isc <isaias.carvalho986@gmail.com>
2025-12-05 09:43:28 -0300

    improve pipelines

--- modules/organization/project/pipelines.tf
+++ modules/organization/project/pipelines.tf
@@ -2,12 +2,12 @@ resource "harness_platform_pipeline" "ci_pipeline" {
   identifier = "ci_build_push"
   org_id     = var.organization_id
   project_id = var.project
-  name       = "DEVOPS - CONTINUOUS INTEGRATION"
+  name       = "DEVOPS - CONTINUOUS INTEGRATION GOLANG"
   
-  yaml = templatefile("${path.module}/templates/devops-continuous-integration.yaml", {
+  yaml = templatefile("${path.module}/templates/tracked-based-go-github/devops-continuous-integration.yaml", {
     PROJECT_ID_PLACEHOLDER = var.project,
     ORG_ID_PLACEHOLDER     = var.organization_id,
-    PIPELINE_NAME          = "DEVOPS - CONTINUOUS INTEGRATION"
+    PIPELINE_NAME          = "DEVOPS - CONTINUOUS INTEGRATION GOLANG"
     PIPELINE_IDENTIFIER    = "ci_build_push"
   })
   depends_on = [ harness_platform_project.ck_project ]
@@ -17,12 +17,12 @@ resource "harness_platform_pipeline" "cd_pipeline" {
   identifier = "cd_deploy"
   org_id     = var.organization_id
   project_id = var.project
-  name       = "DEVOPS - CONTINUOUS DELIVERY"
+  name       = "DEVOPS - CONTINUOUS DELIVERY GOLANG"
   
-  yaml = templatefile("${path.module}/templates/devops-continuous-delivery.yaml", {
+  yaml = templatefile("${path.module}/templates/tracked-based-go-github/devops-continuous-delivery.yaml", {
     PROJECT_ID_PLACEHOLDER = var.project,
     ORG_ID_PLACEHOLDER     = var.organization_id,
-    PIPELINE_NAME          = "DEVOPS - CONTINUOUS DELIVERY"
+    PIPELINE_NAME          = "DEVOPS - CONTINUOUS DELIVERY GOLANG"
     PIPELINE_IDENTIFIER    = "cd_deploy"
   })
   depends_on = [ harness_platform_project.ck_project ]
--- modules/organization/project/triggers.tf
+++ modules/organization/project/triggers.tf
@@ -2,11 +2,11 @@ resource "harness_platform_triggers" "pr_trigger" {
   identifier = "devops_github_pr_trigger"
   org_id     = var.organization_id
   project_id = harness_platform_project.ck_project.id
-  name       = "DEVOPS - GITHUB PR TRIGGER"
+  name       = "DEVOPS - GITHUB PR TRIGGER GOLANG"
   target_id  = harness_platform_pipeline.ci_pipeline.identifier
   
-  yaml = templatefile("${path.module}/templates/devops-github-pr-trigger.yaml", {
-    TRIGGER_NAME        = "DEVOPS - GITHUB PR TRIGGER"
+  yaml = templatefile("${path.module}/templates/tracked-based-go-github/devops-github-pr-trigger.yaml", {
+    TRIGGER_NAME        = "DEVOPS - GITHUB PR TRIGGER GOLANG"
     TRIGGER_IDENTIFIER  = "devops_github_pr_trigger"
     PROJECT_ID          = harness_platform_project.ck_project.id
     ORG_ID              = var.organization_id
