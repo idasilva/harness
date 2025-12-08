f82f5a5c3759abea6213ef034a14d86553ae8d19
isc <isaias.carvalho986@gmail.com>
2025-12-08 16:16:10 -0300

    add a small change to trigger the piepline

--- modules/organization/project/triggers.tf
+++ modules/organization/project/triggers.tf
@@ -39,3 +39,44 @@ resource "harness_platform_triggers" "merge_trigger" {
   
   depends_on = [harness_platform_pipeline.cd_pipeline]
 }
+
+
+resource "harness_platform_triggers" "pr_trigger_java" {
+  identifier = "devops_github_pr_trigger_java"
+  org_id     = var.organization_id
+  project_id = harness_platform_project.ck_project.id
+  name       = "DEVOPS - GITHUB PR TRIGGER JAVA"
+  target_id  = harness_platform_pipeline.ci_pipeline.identifier
+  
+  yaml = templatefile("${path.module}/templates/tracked-based-java-github/triggers/devops-github-pr-trigger.yaml", {
+    TRIGGER_NAME        = "DEVOPS - GITHUB PR TRIGGER JAVA"
+    TRIGGER_IDENTIFIER  = "devops_github_pr_trigger_java"
+    PROJECT_ID          = harness_platform_project.ck_project.id
+    ORG_ID              = var.organization_id
+    PIPELINE_ID         = harness_platform_pipeline.ci_pipeline.identifier
+    GITHUB_CONNECTOR    = "github_main"
+    TARGET_BRANCH       = "main"
+  })
+  
+  depends_on = [harness_platform_pipeline.ci_pipeline]
+}
+
+resource "harness_platform_triggers" "merge_trigger_java" {
+  identifier = "devops_github_merge_trigger_java"
+  org_id     = var.organization_id
+  project_id = harness_platform_project.ck_project.id
+  name       = "DEVOPS - GITHUB MERGE TRIGGER JAVA"
+  target_id  = harness_platform_pipeline.cd_pipeline.identifier
+  
+  yaml = templatefile("${path.module}/templates/tracked-based-java-github/triggers/devops-github-merge-trigger.yaml", {
+    TRIGGER_NAME        = "DEVOPS - GITHUB MERGE TRIGGER JAVA"
+    TRIGGER_IDENTIFIER  = "devops_github_merge_trigger_java"
+    PROJECT_ID          = harness_platform_project.ck_project.id
+    ORG_ID              = var.organization_id
+    PIPELINE_ID         = harness_platform_pipeline.cd_pipeline.identifier
+    GITHUB_CONNECTOR    = "github_main"
+    TARGET_BRANCH       = "main"
+  })
+  
+  depends_on = [harness_platform_pipeline.cd_pipeline]
+}
