b239f1c5a827e5d031757cc3c9d915fdea9701b7
isc <isaias.carvalho986@gmail.com>
2026-01-08 09:20:24 -0300

    avoid expose secrets

--- ci/self-hosted/aws/delegate.sh
+++ ci/self-hosted/aws/delegate.sh
@@ -2,8 +2,8 @@ docker run -d --network host \
   -e DELEGATE_NAME=docker-delegate \
   -e NEXT_GEN="true" \
   -e DELEGATE_TYPE="DOCKER" \
-  -e ACCOUNT_ID=9leDao-yRqu7DN66CVDAeg \
-  -e DELEGATE_TOKEN=NGVmYzQ5MjhjYzVhY2ZlODlkNzcyNzhkM2ZhZTFhMWI= \
+  -e ACCOUNT_ID=XPTO \
+  -e DELEGATE_TOKEN=XPTO= \
   -e DELEGATE_TAGS="" \
   -e MANAGER_HOST_AND_PORT=https://app.harness.io us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.12.87402
 
