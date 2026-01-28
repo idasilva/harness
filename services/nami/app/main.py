2aad76c55b42b9afe9e195986817f58d8cdbe464
idasilva <55162865+idasilva@users.noreply.github.com>
2026-01-28 11:09:40 -0300

    Update root endpoint message in main.py

--- services/nami/app/main.py
+++ services/nami/app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World v2...."}
+    return {"message": "Hello World v2....3333"}
 
 
 @app.get("/python/v2/version")
